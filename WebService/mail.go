76fc25aa7080935b6d29356be1ec5d1d347ee3fb
Aelar <benja.piat@gmail.com>
2017-10-24 17:15:27 +0200

    web service

---
+++ WebService/mail.go
@@ -0,0 +1,100 @@
+package main
+
+
+ import(
+	"fmt"
+	"net/http"
+	//"container/list"
+
+	"github.com/gorilla/mux"
+
+  "math/rand"
+	"golang.org/x/oauth2"
+ "golang.org/x/oauth2/google"
+ "google.golang.org/api/gmail/v1"
+ "log"
+ //"strings"
+ //"encoding/base64"
+ "io/ioutil"
+)
+
+var gmailService gmail.Service
+
+func root(w http.ResponseWriter, r *http.Request) {
+  //vars := mux.Vars(r)
+  //id := vars["id"]
+
+
+  fmt.Fprintf(w, "Hello world !")
+}
+
+func mail(w http.ResponseWriter, r *http.Request) {
+  //vars := mux.Vars(r)
+  //id := vars["id"]
+
+
+  fmt.Fprintf(w, "Send an id")
+}
+
+func mailid(w http.ResponseWriter, r *http.Request) {
+  //vars := mux.Vars(r)
+  //id := vars["id"]
+	rnd := rand.Int() % 15
+	//list := list.New()
+	fmt.Println("Nb unread mail", rnd)
+
+  fmt.Fprintf(w, "%d",rnd)
+}
+
+func initGmail(){
+
+	secret, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	conf, err := google.ConfigFromJSON(secret, gmail.GmailSendScope)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// Creates a URL for the user to follow
+	url := conf.AuthCodeURL("CSRF", oauth2.AccessTypeOffline)
+	// Prints the URL to the terminal
+	fmt.Printf("Visit this URL: \n %v \n", url)
+
+	// Grabs the authorization code you paste into the terminal
+	var code string
+	_, err = fmt.Scan(&code)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// Exchange the auth code for an access token
+	tok, err := conf.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+
+	// Create the *http.Client using the access token
+	client := conf.Client(oauth2.NoContext, tok)
+
+	// Create a new gmail service using the client
+	gmailService, err := gmail.New(client)
+	_ = gmailService
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+  r.HandleFunc("/",root)
+  r.HandleFunc("/mail",mail)
+	r.HandleFunc("/mail/{id}", mailid )
+  http.Handle("/",r)
+
+
+  http.ListenAndServe(":80", nil) // set listen port
+
+}
