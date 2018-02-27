a2960252218694ef582f3af45405dfcadca02c5c
madkira <enlil.baal@gmail.com>
2018-02-27 23:01:06 +0000

    add new version

--- raspberry/script_read.py
+++ raspberry/script_read.py
@@ -2,24 +2,31 @@
 
 import time
 import subprocess # import call as system
+import paho.mqtt.client as paho
 
 # import urllib.request
 from os.path import isfile
 
-users = {
-        "04 71 18 12 02 35 80 \r\n" : "Jean-Yves",
-        "04 71 \r\n" : "Stephane"
-        }
-current = ""   
-try:
+brocker="localhost"
 
-    print("Initialise")
-    message="Initialisation de la clef d'or." 
-    #system(["./dit.sh", message])
-    # init chain of leds
-    #why did we need to do that
-    stack = "";
+def on_message(client, userdata, message):
+    time.sleep(1)
+    subprocess.run(["./dit.sh",str(message.payload.decode("utf-8"))])
+
+client= paho.Client("client-001")
+client.on_message=on_message
+client.connect(brocker)
+client.loop_start()
+
+client.subscribe("goldenkey/exit")
+print("Initialise")
+message="Initialisation de la clef d'or." 
+subprocess.run(["./dit.sh", message])
+# init chain of leds
+#why did we need to do that
+try :
     while 1 :
+        '''
         result = subprocess.run(['./desfirefuncs',''], stdout=subprocess.PIPE).stdout.decode('utf-8')
         if(result) :
             #print(result)
@@ -31,7 +38,6 @@ try:
             if(current!="") :
                 subprocess.Popen(["mosquitto_pub -h localhost -t goldenkey/exit -m \"%s\"" % current], shell = True)
                 current = ""
-        ''' 
         if grovepi.digitalRead(SW) :
             message =' '.join(["Bon retour parmi nous", user, "; vous"])
             url = 'http://192.168.1.190/whoishere/add'
@@ -54,10 +60,13 @@ try:
             urllib2.urlopen(req).read()
             system(["./dit.sh", message])
 '''
-        time.sleep(1)
 except KeyboardInterrupt:
     # reset (all off)
     print ("key error")
     #grovepi.chainableRgbLed_test(LED, numleds, testColorBlack)
 except IOError:
     print ("Error")
+
+
+client.disconnect()
+client.loop_stop()
